faeabc22714ead3d66b2014529594c1314a5257b
afroguts <afifiismail0@gmail.com>
2022-09-02 20:31:00 +0100

    some_changes

---
+++ long_dev_bootcamp/task6.py
@@ -0,0 +1,31 @@
+#Understanding the question is 75% of the answer
+#imposibble to do it this way it's iterating a list in a list with a loop to choose only 2 elements
+
+from re import U
+
+
+list = ["darthtilda", 5 ]
+random_values=[]
+    
+def random_value_consumer(a,z): 
+    for u , x  in (a,z):
+             
+            if x == int(x): 
+              return random_values.append(int(x))
+              
+            elif u == str(u): 
+                return random_values.append(str(u))
+           
+random_value_consumer( (x , u) for a , z in (random_values) )             
+
+    
+
+print (random_values)
+        
+        
+            
+
+
+
+
+
