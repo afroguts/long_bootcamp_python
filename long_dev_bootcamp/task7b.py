eb4b3d5a16b64071be363a08bd40cb9c3c474195
afroguts <afifiismail0@gmail.com>
2022-09-03 18:45:54 +0100

    did it

---
+++ long_dev_bootcamp/task7b.py
@@ -0,0 +1,20 @@
+#Create a new file called python7b.py 
+# and write a new function called save_list_into_file(my_list) 
+# with one parameter which should be a list.
+#After that, check out task 5 again and 
+# save the result of that task (the filtered list) into a new file called my_first_saved_list.txt.
+#1/a function with 1 patrametre == list [] 
+#2/open the  task 5
+#3/save the result the results to a text file
+
+
+def save_list_into_file(my_list=[]):
+    
+    a_list=open(r"C:\Users\ismail\Documents\py first shit\long_dev_bootcamp\my_first_saved_list.txt" , "w") 
+
+    a=a_list.write(str(my_list))
+    a_list.close()
+    print(a)    
+        
+y=[' ', ' ', 'arth', ' ', ' ', ' ', ' ', 'darthtilda']   
+save_list_into_file(y)
