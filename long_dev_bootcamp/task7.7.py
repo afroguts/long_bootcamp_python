af2bb244f5b6b58187078074660b3f52a9d6db55
afroguts <afifiismail0@gmail.com>
2022-09-04 18:08:53 +0100

    trying_import

---
+++ long_dev_bootcamp/task7.7.py
@@ -0,0 +1,11 @@
+#trying to learn import from another file
+
+import task5
+random_list = ["starving", "eru", "oaeu", "ooo"]
+
+a= task5.en_filter("oooooo")
+print(a)
+for y in random_list:
+    b= task5.en_filter(y) 
+
+    print(b)
