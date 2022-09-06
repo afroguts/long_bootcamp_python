a22bd3966b64146b8d6274d2892684cbc64ab545
afroguts <afifiismail0@gmail.com>
2022-09-06 11:55:02 +0100

    same

--- long_dev_bootcamp/task5.py
+++ long_dev_bootcamp/task5.py
@@ -1,7 +1,7 @@
 #Write a Python function with the name "en_filter" which takes one parameter and checks whether a string has no "e" and "n".
 #Use a list comprehension which loops through the previous list from task 3 (community_members) and call (use) that function in the list comprehension.
-
-community_members = ["starving", "pinsaregood", "arth", "blazertherazer", "snow", "tess", "morne", "darthtilda"]
+if __name__ == "__main__":
+    community_members = ["starving", "pinsaregood", "arth", "blazertherazer", "snow", "tess", "morne", "darthtilda"]
 
 
 def en_filter (x):
