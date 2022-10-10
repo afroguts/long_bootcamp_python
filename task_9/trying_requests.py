367c9132d2086e590b3ee0a96c9224059797df07
unknown <afifiismail0@gmail.com>
2022-10-10 15:22:39 +0100

    using_find_bs4

--- task_9/trying_requests.py
+++ task_9/trying_requests.py
@@ -4,21 +4,21 @@
 #find a way to count all "e"s in that whole HTML code and print the number of counts.
 #Do the same for the word "<p>" and print the number of counts as well.
 #print(page.status_code ) if the code printed start with 2 all is working
-#we can use the "xml parser" istead of the lxml but it's old .
-from bs4 import BeautifulSoup
+#we can use the "xml parser" istead of the lxml but it's old and slower.
+from bs4 import BeautifulSoup as bs
 import requests 
 url="https://quotes.toscrape.com"
 file_name= "quotes_html_file.txt"
 if __name__ == "__main__":
     page = requests.get(url)
-    soup = BeautifulSoup(page.text , "lxml")
+    soup = bs(page.text , "lxml")
     with open(file_name , "r+" , encoding="utf-8") as txt_file :
         txt_file.write(f'{soup.prettify()}\n')
-
+        counting_p = len(soup.find_all("p"))
     with open(file_name , "r+" , encoding="utf-8") as txt_file2 :
         text = txt_file2.read()
         counting_e = text.count("e")
-        counting_p = text.count("p")
-        print (f"there are {counting_e} e and {counting_p} p in this file")
+
+        print (f"there are {counting_e} e and {counting_p} <p> in this file")
     
 
